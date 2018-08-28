02b4b6293046eba98fa85867435100a97c5ba08f
Willy Angel <gguillermix@gmail.com>
2018-08-29 01:06:08 +0200

    Rainbow finished

---
+++ Assets/AdultLink/RadialProgressBar/Examples/Scripts/Rainbow.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum StripStatus {
+	filling,
+	filled,
+	shrinking,
+	waiting,
+}
+public class Rainbow : MonoBehaviour {
+
+	// Use this for initialization
+	public RainbowStrip[] strips;
+	private StripStatus stripStatus;
+	private float timeFilled = 0f;
+	private float timeShrunk = 0f;
+	public float fillTime = 3f;
+	public float waitTime = 3f;
+	private int fillCount = 0;
+	private int shrinkCount = 0;
+	void Start () {
+		stripStatus = StripStatus.filled;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (stripStatus == StripStatus.filled) {
+			if (Time.time - timeFilled >= fillTime) {
+				startShrinkSequence();
+			}
+		}
+
+		if (stripStatus == StripStatus.waiting) {
+			if (Time.time - timeShrunk >= waitTime) {
+				startFillSequence();
+			}
+		}
+	}
+
+	private void startFillSequence() {
+		stripStatus = StripStatus.filling;
+		for (int i = 0; i < strips.Length; i++) {
+			strips[i].startFillSequence();
+		}
+	}
+
+	private void startShrinkSequence() {
+		stripStatus = StripStatus.shrinking;
+		for (int i = 0; i < strips.Length; i++) {
+			strips[i].startShrinkSequence();
+		}
+	}
+
+	public void reportFilled() {
+		fillCount++;
+		if (fillCount >= strips.Length) {
+			fillCount = 0;
+			stripStatus = StripStatus.filled;
+			timeFilled = Time.time;
+		}
+	}
+
+	public void reportShrunk() {
+		shrinkCount++;
+		if (shrinkCount >= strips.Length) {
+			shrinkCount = 0;
+			stripStatus = StripStatus.waiting;
+			timeShrunk = Time.time;
+		}
+	}
+}
---
+++ Assets/AdultLink/RadialProgressBar/Examples/Scripts/RainbowStrip.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowStrip : MonoBehaviour {
+
+	private StripStatus stripStatus;
+	private Material mat;
+	public float fillSpeed = 5f;
+	private float fillPercentage = 0f;
+	private Vector3 initialScale;
+	private Vector3 scale;
+	public float scaleAmount = 0.15f;
+	public float scaleThreshold = 0.1f;
+	public Rainbow rainbowController;
+
+	void Start()
+	{
+		fillSpeed /= 1000;
+		mat = GetComponent<Renderer>().material;
+		initialScale = transform.localScale;
+		scale = initialScale;
+		stripStatus = StripStatus.filled;
+	}
+	void FixedUpdate() {
+
+		if (stripStatus == StripStatus.filling) {
+			fillPercentage += fillSpeed;
+			fillPercentage = Mathf.Clamp(fillPercentage, 0f, 1f);
+			mat.SetFloat("_Fillpercentage", fillPercentage);
+			if (fillPercentage >= 1f) {
+				stripStatus = StripStatus.filled;
+				rainbowController.reportFilled();
+			}
+		}
+
+		else if (stripStatus == StripStatus.shrinking) {
+
+			scale.Scale(Vector3.one*(1-scaleAmount));
+			scale = new Vector3(Mathf.Clamp(scale.x, 0f, initialScale.x), Mathf.Clamp(scale.y, 0f, initialScale.y), Mathf.Clamp(scale.z, 0f, initialScale.z));
+			transform.localScale = scale;
+			if (Vector3.Magnitude(scale) < scaleThreshold) {
+				stripStatus = StripStatus.waiting;
+				rainbowController.reportShrunk();
+			}
+		}
+	}
+
+	public void startFillSequence() {
+		fillPercentage = 0;
+		mat.SetFloat("_Fillpercentage", 0f);
+		scale = initialScale;
+		transform.localScale = scale;
+		stripStatus = StripStatus.filling;
+	}
+
+	public void startShrinkSequence() {
+		stripStatus = StripStatus.shrinking;
+	}
+}
